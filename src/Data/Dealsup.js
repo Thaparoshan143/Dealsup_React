e8fe9811bd2f416cf1d7c07f992024c1ecf7f947
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-07-10 23:18:59 +0545

    NavItem and Links Data Added

---
+++ src/Data/Dealsup.js
@@ -0,0 +1,30 @@
+export const _SocialLinks = [
+    {
+        social : "Facebook",
+        url : "",
+    },
+    {
+        social : "Instagram",
+        url : "",
+    },
+    {
+        social : "Twitter",
+        url : "",
+    }
+]
+
+export const _QuickLinks = [
+    {
+        title : "Visit Store",
+        url : "",
+    },
+    {
+        title : "Explore",
+        url : "",
+    },
+    {
+        title : "Top List",
+        url : "",
+    }
+]
+
---
+++ src/Data/MenuItems.js
@@ -0,0 +1,26 @@
+export const _NavItems = [
+    {
+        title: "Home",
+        url : "./home",
+    },
+    {
+        title: "Vendor",
+        url : "./vendor",
+    },
+    {
+        title: "Suppliers",
+        url : "./suppliers",
+    },
+    {
+        title: "Explore",
+        url : "./explore",
+    },
+    {
+        title: "Know More",
+        url : "./knowmore",
+    },
+    {
+        title: "Log in",
+        url : "./login",
+    }
+]
