63617fb5baabda52490676d7033668be02a6aa4a
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-07-12 09:01:26 +0545

    Explore More section added

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,21 +1,60 @@
 import "./Landing.css";
 import "../uni.css";
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaShoppingBasket } from 'react-icons/fa';
+import { useState } from "react";
+import { _OutlineInfo } from "../Data/Dealsup";
 
 function Landing() {
+
+  const [prodName, setProdName] = useState("");
+
+  function updateProdName(name)
+  {
+    console.log(name);
+    setProdName(name);
+  }
+
+  function searchProduct()
+  {
+    let alertMsg = "Searching work on progress. Your product name is : " + prodName;
+    alert(alertMsg);
+  }
+
   return (
     <>
-    <div className="page Landing flex-c-se-c">
+    <div className="Landing flex-c-se-c">
         <div className="bg"></div>
-        <h1 className="theme-text">Dealsup</h1>
-        <div className="Search-Wrapper">
-            <div className="flex-r-sb-c">
-                <input type="text" placeholder="Search product that you want here.."/>
-                <a  href={"#"} className="flex-r-se-c"><FaSearch className="Search-Icon" /></a>
-            </div>
+        <div className="Search-Section page flex-c-se-c">
+          <h1 className="theme-text">Dealsup</h1>
+          <div className="Search-Wrapper">
+              <div className="flex-r-sb-c">
+                  <input type="text" placeholder="Search product that you want here.." onChange={(e)=>updateProdName(e.target.value)}/>
+                  <a onClick={searchProduct} className="flex-r-se-c"><FaSearch className="Search-Icon" /></a>
+              </div>
+          </div>
+          <a className="theme-button" href={"#More"}>Explore More</a>
         </div>
-
-        <a className="theme-button" href={"./home"}>Explore More</a>
+        <div className="Explore-More page flex-c-se-c" id="More">
+          <h1>What's in Dealsup</h1>
+          <div className="More-Info">
+            <h3>As Vendor : </h3>
+            <p>Seek for the supplier that you want for your product and bid the price for buying.</p>
+            <h3>As Suppliers : </h3>
+            <p>Seek for the Vendors that you want to deliver your product and set the price for selling.</p>
+          </div>
+          <div className="flex-r-se-c Outline-Info">
+            {_OutlineInfo.map(({title,size})=>{
+              return (
+                <div className="Info-Card flex-c-se-c">
+                  <FaShoppingBasket style={{fontSize:"8rem"}}/>
+                  <h2 className="pCase theme-text">{title}</h2>
+                  <h2>{size}</h2>
+                </div>
+              )
+            })}
+          </div>
+          <p>Ideal marketplace for both buyers and seller to meet their smooth out their business</p>
+        </div>
     </div>
     </>
   )
--- src/Data/Dealsup.js
+++ src/Data/Dealsup.js
@@ -40,6 +40,17 @@ export const _QuickLinks = [
 ]
 
 export const _Description = [
-    "Dealsup is an online shopping platform that provides great deals and discounts on a wide range of products, making it an ideal place for savvy shoppers to get the best value for their money. Ideal choice for both seller and buyers"
+    "Dealsup is an online marketplace that provides portal on a wide range of products, making it an ideal place for both supplier and vendors to get the best value for their money. Ideal choice for both Vendor and Suppliers to meet their demands."
+]
+
+export const _OutlineInfo = [
+    {
+        title : "Total Vendors",
+        size : "15 +",
+    },
+    {
+        title : "Total Suppliers",
+        size : "20 +",
+    }
 ]
 
