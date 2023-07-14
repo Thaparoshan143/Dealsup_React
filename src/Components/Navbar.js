eb009f4a2285aeaa683bd56fcc98cd911b131a7d
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-07-14 22:12:50 +0545

    mobile responsive added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,20 +3,44 @@ import "./Navbar.css";
 import { _NavItems } from "../Data/MenuItems";
 import { NavLink } from "react-router-dom";
 import dealsuplogo from "../assets/ dealsup-logo.jpg"
+import { useState } from "react";
+import { FaBars, FaMinus } from "react-icons/fa";
 
 function Navbar()
 {
+	const [isOpen, setIsOpen] = useState(false);
+	
+	function changeState()
+	{
+		setIsOpen(pre=>!pre);
+	}
+
 	return(
-		<nav className="Navbar flex-r-sb-c">
-				<a className="Logo-Cont" href={"./landing"}>
-					<img className="Logo" src={dealsuplogo} alt="brand logo" />
-				</a>
+	<>
+		<div className="Mobile-Btn" onClick={changeState}>
+			{!isOpen?<FaBars />:<FaMinus />}
+		</div>
+		{isOpen ?<nav className="Mobile Navbar flex-r-sb-c">
+			<a className="Logo-Cont" href={"./landing"}>
+				<img className="Logo" src={dealsuplogo} alt="brand logo" />
+			</a>
+			<div className="Nav-Wrapper flex-r-se-c">
+				{_NavItems.map(({title, url})=>{
+						return <NavItem title={title} url={url} />
+				})}
+			</div>
+		</nav>:<></>}
+		<nav className="Desktop Navbar flex-r-sb-c">
+			<a className="Logo-Cont" href={"./landing"}>
+				<img className="Logo" src={dealsuplogo} alt="brand logo" />
+			</a>
 			<div className="Nav-Wrapper flex-r-se-c">
 				{_NavItems.map(({title, url})=>{
 						return <NavItem title={title} url={url} />
 				})}
 			</div>
 		</nav>
+	</>
 	)
 }
 
